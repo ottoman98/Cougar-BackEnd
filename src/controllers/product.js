bfaa842d6a6022e69e4743cbfbd6c23aeb946f01
= <=>
2023-03-22 19:30:09 -0500

    commit infernal

---
+++ src/controllers/product.js
@@ -0,0 +1,35 @@
+import productSchema from "../models/productSchema.js";
+
+
+const productcontroller = (req, res) => {
+    const product = productSchema(req.body);
+
+
+    if (req.files) {
+        const filesname = req.files;
+        const files = req.files;
+        const fileUrls = [];
+        files.forEach((file) => {
+            const fileUrl = `${req.protocol}://${req.hostname}:${process.env.PORT}/uploads/${file.filename}`;
+            const tra = req.protocol;
+            fileUrls.push(JSON.stringify(fileUrl));
+        });
+        console.log(fileUrls);
+
+
+
+
+
+        product.imgUrls = fileUrls;
+
+
+    }
+    product
+        .save()
+        .then((data) => { res.send(data); })
+        .catch((e) => { res.json({ error: e }); });
+
+
+};
+
+export default productcontroller;
