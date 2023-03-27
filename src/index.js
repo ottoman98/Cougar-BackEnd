90c58fe0da7f081b765558c41fdc5824642c43e1
= <=>
2023-03-27 11:36:52 -0500

    asdasd

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 //staticFiles
 
 app.use('/imgs', express.static(path.join(__dirname, 'public', 'uploads')));
-console.log(path.join(__dirname, 'public', 'uploads'));
+console.log('asdasdasdad');
 
 
 
