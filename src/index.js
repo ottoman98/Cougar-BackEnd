580b383a1cba38f8ff0d59cc21ad313225305a6b
= <=>
2023-04-03 20:32:33 -0500

    tra

--- src/index.js
+++ src/index.js
@@ -15,8 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 //middelware
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
 
 
 
