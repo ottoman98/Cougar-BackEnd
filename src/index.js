cd181ce46d9ab86380e8113a9f8dbd9c203168e7
= <=>
2023-04-03 20:33:33 -0500

    lean

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 //middelware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 
