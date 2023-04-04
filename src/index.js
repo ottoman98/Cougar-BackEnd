95ef48acf6beca38d8eb5d76c31dd2041fa94ae9
= <=>
2023-04-04 02:45:16 -0500

    siuu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import router from "./routes/products.js";
 import dotenv from "dotenv";
 import path from "path";
 import cors from 'cors';
+
 //connection
 import mongoConnection from "./databases/connection.js";
 
@@ -15,8 +16,16 @@ const PORT = process.env.PORT || 3000;
 
 //middelware
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+app.use(function (err, req, res, next) {
+    if (err instanceof multer.MulterError) {
+        console.log('Error de Multer:', err.message);
+    } else {
+        console.log('Error desconocido:', err);
+    }
+});
+
+
 
 
 
